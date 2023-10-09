7cc5def9dcb679b0a7b9df90187a18954c806f7b
Replit user <>
2023-10-09 01:17:03 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -108,6 +108,11 @@ Node* DFS(Node* initial, int* cont){
     Node* current = top(stack);
     pop(stack);
     (*cont)++;
+
+    if (is_final(current)) {
+      return current;
+    }
+    
   }
   
 
